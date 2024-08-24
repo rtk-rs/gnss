973ed520bb35b77c4e6a4c9ae32e982acdf61e39
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-08-24 09:50:40 +0200

    set_crc32 does work
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- planetary/src/main.rs
+++ planetary/src/main.rs
@@ -8,7 +8,7 @@ use std::path::PathBuf;
 
 use anise::{
     //constants::celestial_objects::EARTH,
-    constants::frames::EARTH_J2000,
+    constants::frames::{EARTH_J2000, EARTH_ITRF93},
     prelude::MetaAlmanac,
     structure::planetocentric::{ellipsoid::Ellipsoid, PlanetaryData},
 };
@@ -26,15 +26,26 @@ fn main() {
     let gps_wgs84 = ellipsoid(298.257223563_f64, 6378.137_f64, 6356.7523142_f64);
 
     let mut almanac =
-        MetaAlmanac::latest().unwrap_or_else(|e| panic!("MetaAlmanac::latest() error: {}", e));
+        MetaAlmanac::latest()
+        .unwrap_or_else(|e| panic!("MetaAlmanac::latest() error: {}", e));
+   
 
     // define new frames
     for (frame_id, frame_name, shape) in [(3990001, "GPS WGS84", gps_wgs84)] {
+        //let earth_itrf93 = EARTH_ITRF93;
+        //let planetary_data = almanac
+        //    .planetary_data
+        //    .get_by_name("GPS WGS84")
+        //    .unwrap_or_else(|e| panic!("get_by_name({}) failure: {}", "GPS WGS84", e));
+        
         let mut earth_inertial = almanac
             .frame_from_uid(EARTH_J2000)
             .unwrap_or_else(|e| panic!("Failed to retrieve Earth from_uid: {}", e));
 
         earth_inertial.shape = Some(shape);
+
+        //pck.get_by_name("GPS WGS84")
+        //    .unwrap_or_else(|e| panic!("oops: {}", e));
 
         let planetary_data = PlanetaryData {
             parent_id: 0,
@@ -58,6 +69,9 @@ fn main() {
     }
     // finally: save the new dataset so we can use it later on
     let path = PathBuf::new().join("pck_gnss.pca");
+
+    almanac.planetary_data.set_crc32();
+
     almanac
         .planetary_data
         .save_as(&path, true)
@@ -72,11 +86,34 @@ fn main() {
 #[cfg(test)]
 mod test {
     use anise::prelude::Almanac;
+    use anise::structure::PlanetaryDataSet;
+    use std::fs::read_to_string;
+    use std::fs::File;
+    use std::io::Read;
     // This test requires main to run once before... :)
     #[test]
     fn test_gnss_pck() {
-        // load gnss_pck.pca
-        let almanac = Almanac::new("pck_gnss.pca")
-            .unwrap_or_else(|e| panic!("Failed to load GNSS Planetary Constants: {}", e));
+        //let mut bytes = Vec::<u8>::new();
+        //// load gnss_pck.pca
+        //let almanac = Almanac::default();
+        //let mut fd = File::open("pck_gnss.pca")
+        //    .unwrap_or_else(|e| panic!("Failed to read GNSS PCA: does file exist? {}", e));
+        //fd.read_to_end(&mut bytes)
+        //    .unwrap_or_else(|e| panic!("Failed to read GNSS PCA: does file exist? {}", e));
+
+        //let pck = PlanetaryDataSet::from_bytes(bytes);
+        //almanac.with_planetary_data(pck);
+        
+        // much simpler, and seems to work
+        let mut almanac = Almanac::default();
+        almanac.load("pck_gnss.pca")
+            .unwrap_or_else(|e| panic!("Failed to read GNSS PCA: does file exist? {}", e));
+
+        // load new definition
+        let gps_wgs84 = almanac
+            .planetary_data
+            .get_by_id(3990001)
+            //.get_by_name("GPS WGS84")
+            .unwrap_or_else(|e| panic!("{}", e));
     }
 }
